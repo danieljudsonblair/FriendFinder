c9987ba8867906261032faa1e7aad900efbd3854
s1191 <danieljudsonblair@gmail.com>
2019-02-02 21:25:16 -0500

    ready for heroku (?)

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,9 @@
-var friendsData = [];
 
-  
-  // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = friendsData;
+module.exports = [
+    {
+        name: "Danimal",
+        image: "https://www.facebook.com/photo.php?fbid=138144086950948&set=picfp.100022660718675&type=3&eid=ARDO2eypnM4o0eyCF74GGdaKZ4oAlot5U_7xTUnaXhx0NtdTskSuGzbhDCbi7eGY0X5WYzgEEM5PMpEe",
+        responses: [1,1,1,1,1,1,1,1,1,1],
+        total: 10
+    }
+];
--- server.js
+++ server.js
@@ -1,35 +1,27 @@
 
-var express = require("express");
-var path = require("path");
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
+// Dependencies
+// =============================================================
+var express = require('express');
 
-// Tells node that we are creating an "express" server
+
+// Sets up the Express App
+// =============================================================
 var app = express();
-
-// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
-
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
-
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
-
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+  console.log('App listening on PORT ' + PORT);
 });
+
+// Routes
+// =============================================================
+// html routes
+require('./app/routing/htmlRoutes.js')(app);
+// api routes
+require('./app/routing/apiRoutes.js')(app);
