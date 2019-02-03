57a89b6d18e4af3ef01c2de4ae330f0b902e1907
s1191 <danieljudsonblair@gmail.com>
2019-02-02 21:59:40 -0500

    minor tweaks.. still needs more I think

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,2 @@
 
-module.exports = [
-    {
-        name: "Danimal",
-        image: "https://www.facebook.com/photo.php?fbid=138144086950948&set=picfp.100022660718675&type=3&eid=ARDO2eypnM4o0eyCF74GGdaKZ4oAlot5U_7xTUnaXhx0NtdTskSuGzbhDCbi7eGY0X5WYzgEEM5PMpEe",
-        responses: [1,1,1,1,1,1,1,1,1,1],
-        total: 10
-    }
-];
+module.exports = [];
