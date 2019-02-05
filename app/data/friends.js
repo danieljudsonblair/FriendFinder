e498c15716a0d376775e6f26efcf5c9f3038d9ea
s1191 <danieljudsonblair@gmail.com>
2019-02-05 10:01:39 -0500

    added starter data to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,15 @@
 
-module.exports = [];
+module.exports = [
+    {
+        name: "Brooke",
+        image: "http://www.mecksheriff.com/getimagefromdbby/getimagefromdbby.asp?booking_number=1775751",
+        responses: [1,1,1,1,1,1,1,1,1,1],
+        total: 10
+    },
+    {
+        name: "Matthew",
+        image: "http://www.mecksheriff.com/getimagefromdbby/getimagefromdbby.asp?booking_number=1775753",
+        responses: [5,4,5,4,3,4,5,2,3,4],
+        total: 39
+    }
+];
